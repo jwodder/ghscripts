7f514b12db7f48d3dd78596d2ddf3cd56d841f9b
John T. Wodder II <git@varonathe.org>
2025-10-20 16:22:40 -0400

    Delint

--- gh-rate-limit.py
+++ gh-rate-limit.py
@@ -6,8 +6,8 @@
 
 from __future__ import annotations
 from datetime import datetime, timezone
-from ghtoken import get_ghtoken
 from ghreq import Client
+from ghtoken import get_ghtoken
 
 with Client(token=get_ghtoken()) as client:
     data = client.get("/rate_limit")
@@ -17,7 +17,8 @@ with Client(token=get_ghtoken()) as client:
             any_used = True
             reset = datetime.fromtimestamp(v["reset"], timezone.utc).astimezone()
             print(
-                f"{k}: {v['used']} / {v['limit']} used; {v['remaining']} left; reset at {reset}"
+                f"{k}: {v['used']} / {v['limit']} used; {v['remaining']} left;"
+                f" reset at {reset}"
             )
     if not any_used:
         print("No rate-limited API requests used")
